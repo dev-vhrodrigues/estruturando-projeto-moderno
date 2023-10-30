c8de262f6a8717ab63686cd4c019a505fb90aed7
dev-vhrodrigues <contato_victorhenrique@hotmail.com>
2023-10-30 11:29:01 -0300

    30/10 - 11h28

--- Exercício 17/webpack.config.js
+++ Exercício 17/webpack.config.js
@@ -17,16 +17,8 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
-        test: /\.(?:js|mjs|cjs)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', { targets: "defaults" }]
-            ]
-          }
-        }
+        test: /.js$/,
+        use: ['babel-loader']
       }
     ]
   },
